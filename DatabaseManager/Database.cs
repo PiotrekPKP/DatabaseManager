0976ca7880f8cec93414dc18f1b870e5d60a196a
Piotrek <peter@LAPTOP-OACQAF8V>
2020-05-24 10:38:56 +0200

    Initial commit

---
+++ DatabaseManager/Database.cs
@@ -0,0 +1,23 @@
+﻿namespace DatabaseManager
+{
+    internal static class Database
+    {
+        private static void Main(string[] args)
+        {
+            //INITIALIZING DATABASE CONNECTION
+            if (!DbManager.Initialize("localhost", "tests", "root", "")) return; //SERVER ERROR
+            
+            //GETTING FROM TABLE EXAMPLE
+            //((Codes)DbManager.Execute("SELECT * FROM codes")[i]).Code; //GIVES THE ith ITEM's Code PROPERTY
+            
+            //INSERTING INTO TABLE EXAMPLE
+            //DbManager.PerformAction("INSERT INTO codes VALUES ('id', 'code')"); //INSERTS id AND code INTO codes TABLE
+            
+            //UPDATING TABLE VARIABLES EXAMPLE
+            //DbManager.PerformAction("UPDATE codes SET code='code' WHERE id='id'"); //UPDATES code VARIABLE IN codes TABLE WHERE id VARIABLE EQUALS id
+            
+            //DELETING ROW FROM TABLE EXAMPLE
+            //DbManager.PerformAction("DELETE FROM codes WHERE id='id'"); //DELETES ROW FROM codes TABLE WHERE id VARIABLE EQUALS id
+        }
+    }
+}
---
+++ DatabaseManager/DbManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace DatabaseManager
+{
+    public static class DbManager
+    {
+        private static MySqlConnection _connection;
+        
+        //FUNCTION FOR INITIALIZING DB CONNECTION
+        public static bool Initialize(string host, string name, string user, string password)
+        {
+            _connection = new MySqlConnection("SERVER=" + host + ";DATABASE=" + name + ";UID=" + user + ";PASSWORD=" + password);
+            return OpenConn();
+        }
+
+        //OPENING CONNECTION
+        private static bool OpenConn()
+        {
+            try
+            {
+                _connection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //EXECUTING QUERIES AND RETURNING OBJECT ARRAY
+        public static DataModel[] Execute(string query)
+        {
+            var cmd = new MySqlCommand(query, _connection);
+            var result = new DataTable();
+            result.Load(cmd.ExecuteReader());
+            
+            if (result.Rows.Count <= 0) return null;
+            switch (result.ToString())
+            {
+                //CODES TABLE EXAMPLE
+                case "codes":
+                    var objects = result.AsEnumerable().Select(
+                        x => new Codes(
+                            int.Parse(x[0].ToString()!),
+                            x[1].ToString())
+                    ).ToArray();
+                    return objects;
+                //TO ADD NEW DATABASE SUPPORT, USE THE FOLLOWING CODE
+                /*case "database_name":
+                    var objects = result.AsEnumerable().Select(
+                        x => new ClassName(
+                            x[0] -> arg0,
+                            x[1] -> arg1,
+                            etc.
+                        )
+                    ).ToArray();
+                    return objects;*/
+                default:
+                    return null;
+            }
+        }
+
+        //EXECUTING QUERIES WITHOUT RETURN VALUE
+        public static bool PerformAction(string query)
+        {
+            var cmd = new MySqlCommand(query, _connection);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
